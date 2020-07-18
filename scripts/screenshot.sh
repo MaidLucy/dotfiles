d20ffb070dd332de6fb851495645e53d0fa2ae09
Lux49 <Lux49@github.com>
2020-07-18 18:43:29 +0200

    add notification for taking a screenshot and a second one for uploading it

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -9,7 +9,8 @@ SECOND_PATH=$SCREENSHOT_DIR/$FILE
 #echo "$FILE_PATH"
 
 slurp | grim -s 1 -c -g - "$FILE_PATH"
-#grim -s 1 -c "$FILE_PATH"
+notify-send "took screenshot and saved to $FILE_PATH"
+
 
 #echo "$SECOND_PATH"
 upload() {
@@ -17,6 +18,7 @@ upload() {
     ln -s "$SECOND_PATH" $WEB_PATH/$UNIQUE_URL.png
     echo https://f.lucy.moe/$UNIQUE_URL
     wl-copy "https://f.lucy.moe/$UNIQUE_URL"
+    notify-send "uploaded to https://f.lucy.moe/$UNIQUE_URL"
 }
 
 $(echo "exit
