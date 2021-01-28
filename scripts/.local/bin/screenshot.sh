7dd2ba53b8e78c699c3cd2f2cfa87862eebb35cb
Lucy <lucy@asagi.moe>
2021-01-28 12:27:00 +0100

    screenshot reworked

--- scripts/.local/bin/screenshot.sh
+++ scripts/.local/bin/screenshot.sh
@@ -6,13 +6,10 @@ FILE_PATH=$(xdg-user-dir PICTURES)/Screenshots/$FILE
 WEB_PATH="/mnt/7TB/web_files/f"
 SCREENSHOT_DIR="/mnt/7TB/Pictures/Screenshots"
 SECOND_PATH=$SCREENSHOT_DIR/$FILE
-#echo "$FILE_PATH"
+CURRENT_OUTPUT=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')
+CURRENT_OUTPUT_SCALE=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .scale')
+echo "$FILE_PATH"
 
-slurp | grim -s 1 -c -g - "$FILE_PATH"
-notify-send "took screenshot and saved to $FILE_PATH"
-
-
-#echo "$SECOND_PATH"
 upload() {
     cp "$FILE_PATH" "$SECOND_PATH"
     ln -s "$SECOND_PATH" $WEB_PATH/$UNIQUE_URL.png
@@ -21,5 +18,30 @@ upload() {
     notify-send "uploaded to https://f.lucy.moe/$UNIQUE_URL"
 }
 
-$(echo "exit
+screenshot(){
+    if [ $1 = "region" ]
+    then
+	    slurp | grim -s $CURRENT_OUTPUT_SCALE -c -g - "$FILE_PATH" && return 0
+    fi
+    if [ $1 = "output" ]
+    then
+	    grim -s $CURRENT_OUTPUT_SCALE -o $CURRENT_OUTPUT "$FILE_PATH" && return 0
+    fi
+    if [ $1 = "everything" ]
+    then
+	    grim -s $CURRENT_OUTPUT_SCALE "$FILE_PATH" && return 0
+    fi
+    return 1
+}
+
+notify(){
+    notify-send "took screenshot and saved to $FILE_PATH" && return 0
+}
+
+prompt(){
+	$(echo "exit 
 upload" | wofi -d)
+}
+
+#slurp | grim -s $CURRENT_OUTPUT_SCALE -c -g - "$FILE_PATH" &&\
+screenshot $1 && notify && prompt
