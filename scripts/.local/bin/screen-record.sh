5f7f9e44a32e24dad4397a4857fda84d07d44e8d
Lucy <lucy@asagi.moe>
2021-07-05 00:05:49 +0200

    improve screen recording script with buffer sizes and having the CRTC ID as an argument

--- scripts/.local/bin/screen-record.sh
+++ scripts/.local/bin/screen-record.sh
@@ -18,14 +18,14 @@ function muh_trep()
 
 time=$(date '+%Y-%m-%d_%H-%M-%S')
 pulse_device="alsa_output.usb-SMSL_SMSL_M6-00.iec958-stereo.monitor"
-crtc_id="60"
+crtc_id=$1
 
 trap muh_trap SIGINT
 
 # execute the processes and add their respective PIDs to the pids array
-ffmpeg -y -hide_banner -framerate 120 -crtc_id $crtc_id -vsync 0 -device /dev/dri/card0 -vaapi_device /dev/dri/renderD128 -f kmsgrab -i - -vf 'hwmap,scale_vaapi=format=nv12' -c:v h264_vaapi -rc_mode 1 -qp 13 -profile:v 100 -level 62 -f mpegts osu-video-"$time".mpegts &
+ffmpeg -y -hide_banner -thread_queue_size 1024 -framerate 60 -crtc_id $crtc_id -vsync 0 -device /dev/dri/card0 -vaapi_device /dev/dri/renderD128 -f kmsgrab -i - -vf 'hwmap,scale_vaapi=format=nv12' -c:v h264_vaapi -rc_mode 1 -qp 13 -profile:v 100 -level 62 -f mpegts osu-video-"$time".mpegts &
 pids+=($!)
-ffmpeg -y -hide_banner -f pulse -i $pulse_device -c:a copy -f wav osu-audio-"$time".wav &
+ffmpeg -y -hide_banner -thread_queue_size 1024 -f pulse -i $pulse_device -c:a copy -f wav osu-audio-"$time".wav &
 pids+=($!)
 
 for pid in ${pids[*]}; do
@@ -33,5 +33,5 @@ for pid in ${pids[*]}; do
 done
 
 # cleanup to be performed after the recording
-ffmpeg -i osu-video-"$time".mpegts -i osu-audio-"$time".wav -c:a copy -c:v copy osu-"$time".mkv
+ffmpeg -i osu-video-"$time".mpegts -i osu-audio-"$time".wav -c:a copy -c:v copy $(xdg-user-dir VIDEOS)/osu-"$time".mkv
 rm osu-video-"$time".mpegts osu-audio-"$time".wav
