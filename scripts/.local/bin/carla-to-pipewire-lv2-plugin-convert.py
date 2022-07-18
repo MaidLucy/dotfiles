445ad36568fd7b8e070adf471fcec4b7ce13f627
Lucy <lucy@asagi.moe>
2022-07-18 13:25:53 +0200

    add script to extract plugin parameters from Carla project files and display them in config snipsets for PipeWire effect sinks/sources

---
+++ scripts/.local/bin/carla-to-pipewire-lv2-plugin-convert.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+path = sys.argv[1]
+if not os.path.exists(path):
+    print('{} does not exist'.format(path))
+    exit()
+
+tree = ET.parse(path)
+root = tree.getroot()
+
+for plugin in tree.findall('Plugin'):
+    uri = plugin.find('Info').find('URI').text
+    name = plugin.find('Info').find('Name').text
+    print('''    {{
+        name = "{}"
+        type = lv2
+        plugin = "{}"
+        control = {{'''.format(name, uri))
+    data = plugin.find('Data')
+    for parameter in data.findall('Parameter'):
+        symbol = parameter.find('Symbol').text
+        value = parameter.find('Value').text
+        print('           "{}" = {}'.format(symbol, value))
+    print('''        }
+    }''')
