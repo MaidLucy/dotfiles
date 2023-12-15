1a501bdffd9248781d675fa1b455b59aa8d53f48
Lucy <maidlucy@pm.me>
2023-12-15 13:48:16 +0100

    sway config changes dies das

--- sway/.config/sway/mako.sh
+++ sway/.config/sway/mako.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 . "${HOME}/.cache/wal/colors.sh"
-mako --output DP-3 --margin 3 --border-size 3 --default-timeout 20000 --background-color "$color0"D7 --text-color "$color7" --border-color "$color2"
+mako --output DP-2 --margin 3 --border-size 3 --default-timeout 20000 --background-color "$color0"D7 --text-color "$color7" --border-color "$color2"
